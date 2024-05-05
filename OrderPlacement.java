93f74006db204ee1ef95c51d0d017f25ff7a80e0
Meldrick Carabeo <meldrick.carabeo@gmail.com>
2024-05-05 18:17:27 +0800

    Create OrderPlacement.java

---
+++ OrderPlacement.java
@@ -0,0 +1,7 @@
+public class OrderPlacerImpl implements OrderPlacer {
+  @Override
+  public void placeOrder(String customerName, String address) {
+    // Simulate placing order in a system
+    System.out.println("Order placed for " + customerName + " at " + address);
+  }
+}
