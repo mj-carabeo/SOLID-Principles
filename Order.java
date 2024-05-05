281f15ebcc8536c641a9acb1a84cfa7601aaa3ec
Meldrick Carabeo <meldrick.carabeo@gmail.com>
2024-05-05 18:07:16 +0800

    Create Order.java

---
+++ Order.java
@@ -0,0 +1,10 @@
+public interface Order {
+
+  void calculateTotal(double price, int quantity);
+
+  void placeOrder(String customerName, String address);
+
+  void generateInvoice(String fileName);
+
+  void sendEmailNotification(String email);
+}
