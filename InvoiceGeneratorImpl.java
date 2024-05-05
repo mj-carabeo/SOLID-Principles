bba7264c2afef0b3da030bc42c99ce61bdabe3a8
Meldrick Carabeo <meldrick.carabeo@gmail.com>
2024-05-05 18:18:10 +0800

    Create InvoiceGeneratorImpl.java

---
+++ InvoiceGeneratorImpl.java
@@ -0,0 +1,7 @@
+public class InvoiceGeneratorImpl implements InvoiceGenerator {
+  @Override
+  public void generateInvoice(String fileName) {
+    // Simulate generating invoice file
+    System.out.println("Invoice generated: " + fileName);
+  }
+}
